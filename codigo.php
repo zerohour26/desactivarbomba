9b33a56989efc31aa13c8e5050f13813fdf2eaa4
zerohour26 <40137031+zerohour26@users.noreply.github.com>
2018-06-10 09:27:03 -0300

    Update codigo.php

--- codigo.php
+++ codigo.php
@@ -1,6 +1,14 @@
 <?php
-$correcto = array('rojo','azul','verde');
-$cable = array('rojo','azul','verde');
+$correcto = $_GET["Correctos"];
+if (empty($_GET["Correctos"]) || $_GET["Correctos"] == null)
+{
+    $correcto = array('rojo','azul','verde');
+}
+$cable = $_GET["Cables"];
+if (empty($_GET["Cables"]) || $_GET["Cables"] == null)
+{
+    $cables = array('rojo','azul','verde');
+}
 $correcto = $correcto[rand(0,count($correcto)-1)];
 echo 'La bomba se activo.';
 $cable = $cable[rand(0,count($correcto)-1)];
