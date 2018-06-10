52e39d30daeb98e0c2e0cbf9a887738a0ddae087
zerohour26 <40137031+zerohour26@users.noreply.github.com>
2018-06-10 10:38:07 -0300

    Update codigo.php

--- codigo.php
+++ codigo.php
@@ -1,11 +1,3 @@
 <?php
-$array1 = array('rojo','azul','verde');
-$correcto = array_rand($array1,2);
-echo 'La bomba se activo.';
-$cable = array_rand($array1,2);
-if ($correcto == $cable) {
-    echo 'Ganaste!';
-} else {
-    echo 'Perdiste!';
-}
+echo 'hi';
 ?>
