ef35cdd6017616fa6fe330451cf660dd030c8e78
zerohour26 <40137031+zerohour26@users.noreply.github.com>
2018-06-10 21:49:27 -0300

    Update codigo.php

--- codigo.php
+++ codigo.php
@@ -1,5 +1,21 @@
 <?php
 namespace App\src;
+class Session
+{
+    static function has($identifier)
+    {
+        return isset($_SESSION[$identifier]);
+    }
+    static function put($identifier, $value)
+    {
+        $_SESSION[$identifier] = $value;
+    }
+    static function pull($identifier)
+    {
+        return $_SESSION[$identifier];
+    }
+}
+namespace App\src;
 use GuzzleHttp\Client;
 use Exception;
 class NightbotAPI
